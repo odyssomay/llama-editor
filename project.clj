7e043857812f0df66ab37b073091cf419b8a8665
Jonathan Fischer Friberg <odyssomay@gmail.com>
2011-07-10 01:48:37 +0200

    cleanup

--- project.clj
+++ project.clj
@@ -1,13 +1,9 @@
 (defproject llama "1.0.0-SNAPSHOT"
   :description "Llama is a lightweight editor for the clojure language."
   :main llama.core
-  :extra-classpath-dirs ["../Hafni/src/"]
-  :dependencies [
-                 [org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/tools.logging "0.1.2"]
                  [hafni "1.0.6-SNAPSHOT"]
                  [clj-arrow "1.0.2-SNAPSHOT"]
                  [seesaw "1.0.7"]
-                 [leiningen "1.6.1"]
-                 ])
-
+                 [leiningen "1.6.1"]])
