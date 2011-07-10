28114bef8072650e77566f31bd640a63da850c7f
Jonathan Fischer Friberg <odyssomay@gmail.com>
2011-07-10 18:26:51 +0200

    added hafni-seesaw to deps

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/tools.logging "0.1.2"]
                  [hafni "1.0.6-SNAPSHOT"]
+                 [hafni-seesaw "1.0.2-SNAPSHOT"]
                  [clj-arrow "1.0.2-SNAPSHOT"]
                  [seesaw "1.0.7"]
                  [leiningen "1.6.1"]])
