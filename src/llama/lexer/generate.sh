ca2b2857231e8783a18b6517352cbb515b31d2a7
Jonathan Fischer Friberg <odyssomay@gmail.com>
2011-07-11 02:22:13 +0200

    fix for previous move

--- src/llama/lexer/generate.sh
+++ src/llama/lexer/generate.sh
@@ -1,5 +1,5 @@
 dir=../../..
 
 java -cp $dir/lib/clojure-1.2.1.jar clojure.main createcljflex.clj
-$dir/jflex/jflex*/bin/jflex clojure.flex
-javac -classpath $dir/src/:$dir/classes/:$dir/jflex/jflex* ClojureLexer.java
+./jflex*/bin/jflex clojure.flex
+javac -classpath $dir/src/:$dir/classes/:./jflex* ClojureLexer.java
