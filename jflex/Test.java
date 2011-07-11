cfa4efeecda6849d5c0283d8d4b1ce2bbf16c558
Jonathan Fischer Friberg <odyssomay@gmail.com>
2011-07-11 02:18:07 +0200

    cleanup

--- jflex/Test.java
+++
@@ -1,7 +0,0 @@
-import llama.Lexer;
-
-public class Test {
-	public static void main(String[] args) {
-		Lexer a = new Lexer();
-	}
-}
--- jflex/generate.sh
+++
@@ -1,4 +0,0 @@
-java -cp ../lib/clojure-1.2.1.jar clojure.main createcljflex.clj
-./jflex*/bin/jflex clojure.flex
-javac -classpath ../src/ ClojureLexer.java
-mv ClojureLexer.class ../src/llama
