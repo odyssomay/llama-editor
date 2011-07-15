c8b2e5fd38fb5d86b66024231b72085ba1f1fa9c
Jonathan Fischer Friberg <odyssomay@gmail.com>
2011-07-16 01:40:49 +0200

    state handled with local dirs

--- src/llama/state.clj
+++ src/llama/state.clj
@@ -3,18 +3,25 @@
   (:import (java.io BufferedInputStream BufferedOutputStream
                     FileInputStream FileOutputStream)))
 
+(def state-file (file "state"))
+(def beans-file (file state-file "beans"))
+
+(doseq [f [state-file beans-file]]
+  (if-not (.exists f)
+    (.mkdir f)))
+
 ;; states
 
 (defn load-state 
   ([id]
    (load-state id identity))
   ([id if-found]
-   (let [f (file (.getPath (ClassLoader/getSystemResource "state")) (name id))]
+   (let [f (file state-file (name id))]
      (if (.exists f)
        (if-found (read-string (slurp f)))))))
 
 (defn save-state [id state]
-  (let [f (file (.getPath (ClassLoader/getSystemResource "state")) (name id))]
+  (let [f (file state-file (name id))]
     (with-open [out (java.io.FileWriter. f)]
       (binding [*out* out]
         (pr state)))))
@@ -33,13 +40,13 @@
   ([id]
    (load-bean id identity))
   ([id if-found]
-   (let [f (file (.getPath (ClassLoader/getSystemResource "beans")) (name id))]
+   (let [f (file beans-file (name id))]
      (if (.exists f)
        (with-open [d (java.beans.XMLDecoder. (BufferedInputStream. (FileInputStream. f)))]
          (if-found (.readObject d)))))))
 
 (defn save-bean [id b]
-  (let [f (file (.getPath (ClassLoader/getSystemResource "beans")) (name id))]
+  (let [f (file beans-file (name id))]
     (with-open [enc (java.beans.XMLEncoder. (BufferedOutputStream. (FileOutputStream. f)))]
       (.writeObject enc b))))
 
