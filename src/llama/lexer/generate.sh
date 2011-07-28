ae24cc93d790fa66f5694a81afdf3a0064b4710b
Jonathan Fischer Friberg <odyssomay@gmail.com>
2011-07-29 00:26:21 +0200

    removed jflex

--- src/llama/lexer/generate.sh
+++ src/llama/lexer/generate.sh
@@ -1,5 +1,5 @@
 dir=../../..
 
 java -cp $dir/lib/clojure-1.2.1.jar clojure.main createcljflex.clj
-$dir/jflex*/bin/jflex clojure.flex
+jflex clojure.flex
 javac -classpath $dir/src/:$dir/classes/ ClojureLexer.java
