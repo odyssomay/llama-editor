0c5d71c951844a083466ab6e54ebd863f15184aa
Jonathan Fischer Friberg <odyssomay@gmail.com>
2011-07-29 13:57:28 +0200

    simplified main.clj, avoids an additional process

--- src/llama/main.clj
+++ src/llama/main.clj
@@ -1,24 +1,6 @@
 (ns llama.main
-  (:gen-class)
-  (:use llama.lib)
-  (:import (java.io IOException InputStreamReader OutputStreamWriter)))
-
-(defn copy [input output]
-  (-> (fn []
-        (try 
-          (.write output (.read input))
-          (recur)
-          (catch IOException _ )))
-      (Thread.)
-      .start))
+  (:gen-class))
 
 (defn -main [& args]
-  (let [process (start-process (str "java -cp " (.getProperty (System/getProperties) "java.class.path" nil)
-                                    " clojure.main -"))]
-    (copy (:input_stream process) *out*)
-    (copy (:error_stream process) *err*)
-    (doto (:output_stream process)
-      (.write "(use 'llama.core) (llama-editor)")
-      .flush)
-    (.waitFor (:process process))
-    (System/exit 0)))
+  (eval '(do (use 'llama.core)
+             (llama-editor))))
