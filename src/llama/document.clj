0b92487809380d28ae75094b8c87438fc9b9326d
Jonathan Fischer Friberg <odyssomay@gmail.com>
2011-08-10 00:49:06 +0200

    initial code folding

--- src/llama/document.clj
+++ src/llama/document.clj
@@ -1,9 +1,10 @@
 (ns llama.document
   (:use (llama 
-          [syntax :only [indent]] 
+          [syntax :only [indent parens-count find-unmatched-rparens]] 
           [lib :only [*available-fonts* color font log]])
-        [clojure.string :only [split]]
-        [seesaw.invoke :only [invoke-later invoke-now]])
+        [clojure.string :only [split split-lines join]]
+        [seesaw.invoke :only [invoke-later invoke-now]]
+        [seesaw.graphics :only [anti-alias style draw polygon circle rect]])
   (:require [seesaw.core :as ssw])
   (:import javax.swing.text.AbstractDocument$DefaultDocumentEvent
            javax.swing.event.DocumentEvent$EventType
@@ -20,6 +21,8 @@
                  "Monospaced" true
                  nil) *available-fonts*)) 12))
 
+;; undo/redo
+
 (defn init-undoable-edits [jdoc]
   (let [manager (javax.swing.undo.UndoManager.)
         listener (reify javax.swing.event.UndoableEditListener
@@ -31,6 +34,8 @@
                                              ))))]
     (.addUndoableEditListener jdoc listener)
     manager))
+
+;; autocompletion
 
 (def clojure-provider (org.fife.ui.autocomplete.DefaultCompletionProvider.))
 
@@ -61,6 +66,8 @@
 (.addCompletion clojure-provider
   (org.fife.ui.autocomplete.BasicCompletion. clojure-provider "def"))
 
+;; highlighting
+
 (defmacro syntax-style [style]
   (symbol (str "org.fife.ui.rsyntaxtextarea.SyntaxConstants/SYNTAX_STYLE_" style)))
 
@@ -73,14 +80,143 @@
 (defn set-syntax-style [area type]
   (.setSyntaxEditingStyle area (type->syntax-style type)))
 
+;; code folding
+
+(defn get-viewport-bounds [viewport line-height]
+  (let [start-pos (.y (.getViewPosition viewport))
+        start-line (quot start-pos line-height)
+        number-of-lines (quot (.height (.getExtentSize viewport))
+                              line-height)]
+    [start-line (+ number-of-lines start-line)]))
+
+(defn draw-block [g line-height state start end]
+  (let [b (.getClipBounds g)
+        y-start (* line-height start)]
+    (.setColor g (case state
+                   :open (color 200 0 0)
+                   :close (color 0 200 0)))
+    (.fillRect g (.x b)     (* line-height start)
+                 (.width b) (* line-height (- end start)))
+    ;(draw g (polygon [(+ (.x b) 1) (inc y-start)] [(+ (.x b) 8) (inc y-start)] [5 (+ y-start 10)])
+    ;      (style :foreground (color 255 255 255)))
+;    (draw g (case state
+;              :open (circle (+ (.x b) 5) (+ y-start 5) 3.5)
+;              :close (rect (inc (.x b)) (inc y-start) 7 7)
+;              nil) (style :foreground (color 255 255 255)
+;                          :background (color 255 255 255)))
+    ))
+
+(defn get-blocks [text]
+  (->> text
+       split-lines
+       (map parens-count)
+       (reductions +)
+       (partition-by zero?)))
+
+(defn get-block-offsets [text]
+  (let [blocks (get-blocks text)]
+    (partition 2
+      (reductions + (map count (if (= (ffirst blocks) 0)
+                                   blocks
+                                   (concat [()] blocks)))))))
+
+(defn init-fold-component [scroll_pane text_area]
+  (let [get-line-height #(.getHeight (.getFontMetrics text_area (.getFont text_area)))
+        c (proxy [javax.swing.JComponent] []
+            (getPreferredSize []
+              (java.awt.Dimension. 10 (.height (.getPreferredSize text_area))))
+            (paintComponent [g]
+              (anti-alias g)
+              (let [b (.getClipBounds g)
+                    line_height (get-line-height)
+                    [start-line end-line] (get-viewport-bounds (.getViewport scroll_pane) line_height)]
+                (.setColor g (color 0 0 0))
+                (.fillRect g (.x b) (.y b) (.width b) (.height b))
+                (doseq [[start end] (filter (fn [[start end]]
+                                              (and (<= start end-line)
+                                                   (>= end start-line))) 
+                                            (get-block-offsets (.getText text_area)))]
+                  (draw-block g line_height :open start (inc end)))
+                (doseq [line (.getFoldedLines (.getDocument text_area))]
+                  (draw-block g line_height :close line (inc line))))))]
+    (.addMouseListener c
+      (proxy [java.awt.event.MouseAdapter] []
+        (mouseClicked [e]
+          (let [clicked-line (quot (.getY e) (get-line-height))
+                line (some (fn [[start end]]
+                             (if (and (>= clicked-line start)
+                                      (<= clicked-line end))
+                               start)) (get-block-offsets (.getText text_area)))
+                d (.getDocument text_area)]
+            (if line
+              (.fold d line)
+              (if (some (partial = clicked-line) (.getFoldedLines d))
+                (.unfold d clicked-line)))))))
+    c))
+
+(defn get-line-offset [text line]
+  (->> text
+       (reductions #(if (= %2 \newline) (inc %1) %1) 0)
+       (take-while #(< % line))
+       count))
+
+(defn get-line-for-offset [text offset]
+  (count (filter (partial = \newline) (take offset text))))
+
+(defn update-folded-text [folded-text offset line-difference full-text]
+  (let [line (get-line-for-offset full-text offset)]
+    (map (fn [fold]
+           (if (> (:line fold) line)
+             (assoc fold :line (+ (:line fold) line-difference))
+             fold))
+         folded-text)))
+
+;; text
+
+(defprotocol FoldableDocument
+  (getFoldedLines [this])
+  (fold           [this line])
+  (unfold         [this line]))
+
 (defn create-clojure-document []
-  (proxy [org.fife.ui.rsyntaxtextarea.RSyntaxDocument] [nil]
-    (insertString [offset text a]
-      (let [indented_text 
-            (if (= text "\n")
-              (indent (.getText this 0 offset))
-              text)]
-        (proxy-super insertString offset indented_text a)))))
+  (let [folded-text (atom [])
+        document (proxy [org.fife.ui.rsyntaxtextarea.RSyntaxDocument llama.document.FoldableDocument] [nil]
+                   (insertString [offset text a]
+                     (let [indented_text 
+                           (if (= text "\n")
+                             (indent (.getText this 0 offset))
+                             text)]
+                       (swap! folded-text 
+                              update-folded-text offset 
+                              (count (filter (partial = \newline) text))
+                              (.getText this 0 (.getLength this)))
+                       (proxy-super insertString offset indented_text a)))
+                   (remove [offset length]
+                     (swap! folded-text
+                            update-folded-text offset
+                            (- (count (filter (partial = \newline) (.getText this offset length))))
+                            (.getText this 0 (.getLength this)))
+                     (proxy-super remove offset length))
+                   ;; folding
+                   (getFoldedLines [] (map :line @folded-text))
+                   (fold [line]
+                     (let [text (.getText this 0 (.getLength this))
+                           start (get-line-offset text line)
+                           subtext (subs text start)
+                           end (+ 2 (find-unmatched-rparens (rest subtext)))
+                           block (subs subtext 0 end)]
+                       (.remove this start end)
+                       (.insertString this start (str (first (split-lines subtext)) " ... )") nil)
+                       (swap! folded-text conj {:line line :text block}))) 
+                   (unfold [line] 
+                     (let [text (.getText this 0 (.getLength this))
+                           start (get-line-offset text line)
+                           subtext (subs text start)
+                           end (+ 2 (find-unmatched-rparens (rest subtext)))]
+                       (.remove this start end)
+                       (.insertString this start (:text (first (filter #(= (:line %) line) @folded-text))) nil)
+                       (swap! folded-text (fn [state] (remove #(= (:line %) line) state))))))]
+    document))
 
 (defn create-text-area [file]
   (let [text (if (:path file) (slurp (:path file)) "")
@@ -104,7 +240,15 @@
       (.setAutoIndentEnabled area false)
       (let [ac (org.fife.ui.autocomplete.AutoCompletion. clojure-provider)]
         (.setShowDescWindow ac true)
-        (.install ac area)))
+        (.install ac area))
+      (let [fold-component (init-fold-component content area)]
+        (.setRowHeaderView content fold-component)
+        (.addDocumentListener document
+          (reify javax.swing.event.DocumentListener
+            (changedUpdate [_ _]
+              (.repaint fold-component))
+            (insertUpdate [_ _] )
+            (removeUpdate [_ _] )))))
     (set-syntax-style area type) 
     (.setFont area (get-font))
     (.setText area text)
