1a18b669c0d1ce912d767a50e2092d9551305b5a
Jonathan Fischer Friberg <odyssomay@gmail.com>
2011-09-05 11:27:45 +0200

    options set/get/listen

--- src/llama/config.clj
+++ src/llama/config.clj
@@ -1,5 +1,6 @@
 (ns llama.config
-  (:use [llama.lib :only [log]]))
+  (:use [llama.lib :only [log]])
+  (:require [seesaw.core :as ssw]))
 
 (defn load-config-files [& files]
   (dorun (map #(load (str "/config/" %)) files)))
@@ -14,3 +15,34 @@
                 (str "[" (slurp (.getPath (ClassLoader/getSystemResource "config-order")))
                      "]")))))
 
+;; options
+
+(def options (atom {}))
+
+(defn get-option* [class-id id]
+  (get-in @options [class-id id]))
+
+(defn set-option [class-id id value]
+  (swap! options assoc-in [class-id id] value))
+
+(defn listen-to-option
+  ([class-id f]
+   (add-watch options (gensym) 
+     (fn [_ _ old-item item]
+       (let [o (get old-item class-id)
+             n (get item class-id)]
+         (if-not (= o n)
+           (f o n))))))
+  ([class-id id f]
+   (listen-to-option class-id
+     (fn [old-item item]
+       (let [o (get old-item id)
+             n (get item id)]
+       (if-not (= o n)
+         (f o n)))))))
+
+(def options-dialog
+  (ssw/frame))
+
+(defn show-options-dialog []
+  (-> options-dialog ssw/pack! ssw/show!))
