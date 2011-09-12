9a0713654f5c3a6e66946184e6f96dc5ee9db098
Jonathan Fischer Friberg <odyssomay@gmail.com>
2011-09-12 18:53:47 +0200

    llama-dir

--- src/llama/statics.clj
+++ src/llama/statics.clj
@@ -1,4 +1,9 @@
 (ns llama.statics
+  (:use [clojure.java.io :only [file]])
   (:require [seesaw.core :as ssw]))
+
+(def llama-dir (file ".llama"))
+(if-not (.exists llama-dir)
+  (.mkdirs llama-dir))
 
 (def frame (ssw/frame))
